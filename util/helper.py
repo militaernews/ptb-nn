97dcc597e748b582c0487a45767523b38403dc23
nyx <loool1912@web.de>
2024-05-18 15:49:57 +0200

    fix

--- util/helper.py
+++ util/helper.py
@@ -1,12 +1,10 @@
-import base64
 import logging
 import os.path
 
-from resvg_py import svg_to_base64
-from telegram import Update, User, Message
-from telegram.constants import ChatType
+from resvg_py import svg_to_bytes
+from telegram import Update, User
 from telegram.error import TelegramError
-from telegram.ext import  ContextTypes
+from telegram.ext import ContextTypes
 
 from config import MSG_REMOVAL_PERIOD, LOG_GROUP
 from constant import FOOTER
@@ -15,7 +13,7 @@ CHAT_ID = "chat_id"
 MSG_ID = "msg_id"
 
 
-async def delete(context:  ContextTypes.DEFAULT_TYPE):
+async def delete(context: ContextTypes.DEFAULT_TYPE):
     await context.bot.delete_message(context.job.data[CHAT_ID], context.job.data[MSG_ID])
 
 
@@ -38,15 +36,15 @@ async def attempt_message_deletion(update: Update):
         print("needs admin:", e)
 
 
-async def log_error(update: Update, file_name: str, e: Exception, ):
-    logging.error(
-
+async def log_error(context: ContextTypes.DEFAULT_TYPE, update: Update, file_name: str, e: Exception, ):
+    await context.bot.send_message(
+        LOG_GROUP,
         f"<b>⚠️ Error when trying to read html-file {file_name}</b>\n<code>{e}</code>\n\n"
         f"<b>Caused by Update</b>\n<code>{update}</code>",
     )
 
 
-async def reply_html(update: Update, context:  ContextTypes.DEFAULT_TYPE, file_name: str):
+async def reply_html(update: Update, context: ContextTypes.DEFAULT_TYPE, file_name: str):
     await attempt_message_deletion(update)
     try:
         text = get_text(update, file_name)
@@ -65,29 +63,28 @@ async def reply_html(update: Update, context:  ContextTypes.DEFAULT_TYPE, file_n
 
         context.job_queue.run_once(delete, MSG_REMOVAL_PERIOD, {CHAT_ID: msg.chat.id, MSG_ID: msg.message_id})
     except Exception as e:
-        await log_error(update, file_name, e)
+        await log_error(context, update, file_name, e)
 
 
-async def reply_photo(update: Update, context:  ContextTypes.DEFAULT_TYPE, file_name: str, caption: str = None):
+async def reply_photo(update: Update, context: ContextTypes.DEFAULT_TYPE, file_name: str, caption: str = None):
     await attempt_message_deletion(update)
     if caption is not None:
         try:
             caption = get_text(update, caption)
         except Exception as e:
-            await log_error(update, file_name, e)
+            await log_error(context, update, file_name, e)
     try:
         with open(f"res/img/{file_name}", "rb") as f:
             msg = await send_photo_based_on_reply(update, context, f, caption)
             context.job_queue.run_once(delete, MSG_REMOVAL_PERIOD, {CHAT_ID: msg.chat.id, MSG_ID: msg.message_id})
     except Exception as e:
-        await log_error(update, file_name, e)
+        await log_error(context, update, file_name, e)
 
 
 def export_svg(svg: str, filename: str):
     logging.info(svg)
-    encoded = svg_to_base64(svg, dpi=300, font_dirs=["./res/fonts"], text_rendering="geometric_precision")
     with open(filename, 'wb') as f:
-        f.write(base64.b64decode(encoded))
+        f.write(bytes(svg_to_bytes(svg_string=svg, dpi=300, font_dirs=["../res/fonts"])))
 
 
 def read_file(path: str) -> str:
@@ -95,9 +92,7 @@ def read_file(path: str) -> str:
         return f.read()
 
 
-
-
-async def send_photo_based_on_reply(update: Update, context:  ContextTypes.DEFAULT_TYPE, photo, caption: str = None):
+async def send_photo_based_on_reply(update: Update, context: ContextTypes.DEFAULT_TYPE, photo, caption: str = None):
     if update.message.reply_to_message is not None:
         return await update.message.reply_to_message.reply_photo(photo, caption=caption)
     else:
