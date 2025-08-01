8d347edfb7a1e8beed46c0c6c5e59918ea3b4b93
nyx <loool1912@web.de>
2025-08-01 07:26:24 +0200

    fixing linkpreview, dependency bump

--- bot/main.py
+++ bot/main.py
@@ -4,30 +4,31 @@ from os import path, makedirs
 from typing import Final
 from warnings import filterwarnings
 
+from telegram import LinkPreviewOptions
 from telegram.constants import ParseMode
 from telegram.ext import MessageHandler, Defaults, ApplicationBuilder, filters, CommandHandler, ChatJoinRequestHandler, \
     InlineQueryHandler, CallbackQueryHandler
 from telegram.warnings import PTBUserWarning
 
-from bot. private.setup   import set_cmd
-from bot. private.advertisement import register_advertisement
 # from channel.crawl_tweet import PATTERN_TWITTER, handle_twitter
-from bot. channel.loss_osint import get_osint_losses, setup_osint_crawl
-from bot. channel.loss_uamod import get_uamod_losses, setup_uamod_crawl
-from bot. channel.meme import register_meme
-from bot. channel.ukraine_russia import register_ua_ru
-from bot. data.db import get_destination_ids, get_accounts
-from bot. group.bingo import bingo_field, reset_bingo
-from bot. group.command import admin, inline_query, unwarn_user, warn_user, report_user, register_commands
-from bot. group.dictionary import handle_other_chats
-from bot. private.feedback import fwd, respond_feedback
-from bot. private.join_request import join_request_buttons, join_request_ug, accept_rules_ug, decline_request_ug, \
+from bot.channel.loss_osint import get_osint_losses, setup_osint_crawl
+from bot.channel.loss_uamod import get_uamod_losses, setup_uamod_crawl
+from bot.channel.meme import register_meme
+from bot.channel.ukraine_russia import register_ua_ru
+from bot.data.db import get_destination_ids, get_accounts
+from bot.group.bingo import bingo_field, reset_bingo
+from bot.group.command import admin, inline_query, unwarn_user, warn_user, report_user, register_commands
+from bot.group.dictionary import handle_other_chats
+from bot.private.advertisement import register_advertisement
+from bot.private.feedback import fwd, respond_feedback
+from bot.private.join_request import join_request_buttons, join_request_ug, accept_rules_ug, decline_request_ug, \
     accept_request_ug
-from bot. private.pattern import add_pattern_handler
-from bot. settings.config import TELEGRAM, ADMINS, ADMIN_GROUP, CONTAINER, UG_LZ, ADMIN_GROUPS
-from bot. source.add import add_source_handler, handle_join
-from bot. source.edit import edit_source_handler
-from bot. source.lookup import lookup
+from bot.private.pattern import add_pattern_handler
+from bot.private.setup import set_cmd
+from bot.settings.config import TELEGRAM, ADMINS, ADMIN_GROUP, CONTAINER, UG_LZ, ADMIN_GROUPS
+from bot.source.add import add_source_handler, handle_join
+from bot.source.edit import edit_source_handler
+from bot.source.lookup import lookup
 
 
 def add_logging():
@@ -61,10 +62,13 @@ def add_logging():
 
 
 def main():
-    app = ApplicationBuilder().token(TELEGRAM).defaults(
-        Defaults(parse_mode=ParseMode.HTML)) \
-        .read_timeout(15).get_updates_read_timeout(50) \
-        .build()
+    defaults = Defaults(parse_mode=ParseMode.HTML, link_preview_options=LinkPreviewOptions(is_disabled=True))
+    app = (ApplicationBuilder()
+           .token(TELEGRAM)
+           .defaults(defaults)
+           .read_timeout(15)
+           .get_updates_read_timeout(50)
+           .build())
 
     #  .persistence(PicklePersistence(filepath="persistence")) \
     app.add_handler(CommandHandler("setup", set_cmd, filters.Chat(ADMINS)))
